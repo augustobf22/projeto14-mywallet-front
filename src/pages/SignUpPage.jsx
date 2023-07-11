0a5bcdf67690a72d50cecb4b9aaaf268edc33779
augustof <augustobfanck@gmail.com>
2023-07-10 21:00:04 -0300

    first commit front

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,90 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import axios from "axios";
+import { useState } from "react";
+
+const viteURL = import.meta.env.VITE_API_URL;
 
 export default function SignUpPage() {
+  const navigate = useNavigate();
+
+  const url = `${viteURL}/cadastro`;
+
+  const [signUp, setSignUp] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+  const [confirmPass, setConfirm] = useState("");
+
+  function submitForm(event) {
+    event.preventDefault();
+
+    if(signUp.password!==confirmPass) {
+      alert("As senhas não estão iguais!");
+      return;
+    };
+
+    const promise = axios.post(url, signUp);
+
+    promise.then(r => {
+      navigate("/");
+    });
+    promise.catch(r => {
+      console.log(r);
+      alert(r.response.data.message);
+    });
+  };
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={submitForm}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input
+          required
+          placeholder="Nome"
+          type="text"
+          value={signUp.name}
+          onChange={e => setSignUp({
+            name: e.target.value,
+            email: signUp.email,
+            password: signUp.password
+          })}
+        />
+        <input 
+          required 
+          placeholder="E-mail" 
+          type="email" 
+          value={signUp.email}
+          onChange={e => setSignUp({
+            name: signUp.name,
+            email: e.target.value,
+            password: signUp.password
+          })}
+        />
+        <input 
+          required 
+          placeholder="Senha" 
+          type="password" 
+          value={signUp.password}
+          onChange={e => setSignUp({
+            name:signUp.name,
+            email: signUp.email,
+            password: e.target.value
+          })}
+        />
+        <input 
+          required 
+          placeholder="Confirme a senha" 
+          type="password" 
+          value={confirmPass}
+          onChange={e => setConfirm(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to="/">
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
